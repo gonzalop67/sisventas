ee24ce2a333532a8a03c8410a99d2681d848f60d
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-06-28 07:59:31 -0500

    Insertar y editar Categorías.

---
+++ resources/views/almacen/categoria/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin')
+@section('contenido')
+<div class="row">
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+        <h3>Listado de Categorías <a href="categoria/create"><button class="btn btn-success">Nuevo</button></a></h3>
+        @include('almacen.categoria.search')
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-condensed table-hover">
+                <thead>
+                    <tr>
+                        <td>Id</td>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th>Opciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categorias as $categoria)
+                        <tr>
+                            <td>{{ $categoria->idcategoria }}</td>
+                            <td>{{ $categoria->nombre }}</td>
+                            <td>{{ $categoria->descripcion }}</td>
+                            <td>
+                                <a href="{{route('editar_categoria', ['id' => $categoria->idcategoria])}}"><button class="btn btn-info">Editar</button></a>
+                                <a href=""><button class="btn btn-danger">Eliminar</button></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        {{$categorias->render()}}
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('almacen/categoria', 'CategoriaController@index')->name('categoria');
+Route::get('almacen/categoria/create', 'CategoriaController@create')->name('crear_categoria');
+Route::post('almacen/categoria', 'CategoriaController@store')->name('guardar_categoria');
+Route::get('almacen/categoria/{id}/edit', 'CategoriaController@edit')->name('editar_categoria');
+Route::put('almacen/categoria/{id}', 'CategoriaController@update')->name('actualizar_categoria');
