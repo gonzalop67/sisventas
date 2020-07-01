8b37326bdc0541b8ae98df16167f52e06df6058f
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-07-01 07:38:56 -0500

    Registro Compras Almacén (2)

--- resources/views/compras/ingreso/create.blade.php
+++ resources/views/compras/ingreso/create.blade.php
@@ -20,7 +20,7 @@
             <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
                 <div class="form-group">
                     <label for="proveedor">Proveedor</label>
-                    <select name="idproveedor" id="idproveedor" class="form-control">
+                    <select name="idproveedor" id="idproveedor" class="form-control selectpicker" data-live-search="true">
                         @foreach ($personas as $persona)
                             <option value="{{$persona->idpersona}}">{{$persona->nombre}}</option>
                         @endforeach
@@ -53,7 +53,59 @@
         <div class="row">
             <div class="panel panel-primary">
                 <div class="panel-body">
-
+                    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+                        <div class="form-group">
+                            <label for="">Producto</label>
+                            <select name="pidproducto" id="pidproducto" class="form-control selectpicker" data-live-search="true">
+                                @foreach ($productos as $producto)
+                                    <option value="{{$producto->idproducto}}">{{$producto->producto}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                        <div class="form-group">
+                            <label for="cantidad">Cantidad</label>
+                            <input type="text" name="pcantidad" id="pcantidad" class="form-control" placeholder="cantidad">
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                        <div class="form-group">
+                            <label for="precio_compra">Precio Compra</label>
+                            <input type="number" name="pprecio_compra" id="pprecio_compra" class="form-control" placeholder="P. Compra">
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                        <div class="form-group">
+                            <label for="precio_venta">Precio Venta</label>
+                            <input type="number" name="pprecio_venta" id="pprecio_venta" class="form-control" placeholder="P. Venta">
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                        <div class="form-group">
+                            <button type="button" id="bt_add" class="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                        <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
+                            <thead style="background-color: #A9D0F5">
+                                <th>Opciones</th>
+                                <th>Producto</th>
+                                <th>Cantidad</th>
+                                <th>Precio Compra</th>
+                                <th>Precio Venta</th>
+                                <th>Subtotal</th>
+                            </thead>
+                            <tfoot>
+                                <th>TOTAL</th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th><h4 id="total">$ 0.00</h4></th>
+                            </tfoot>
+                        </table>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
