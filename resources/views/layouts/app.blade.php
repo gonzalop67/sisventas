988ca94f560230ed7a04f41003aeee4c329dc22c
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-07-02 07:58:18 -0500

    Adición del favicon.ico en la plantilla de login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="shortcut icon" href="{{asset("img/favicon.ico")}}">
 </head>
 <body>
     <div id="app">
