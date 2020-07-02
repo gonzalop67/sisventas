289fa8344b04fbc3d9d172d05657f8c29426ff72
Gonzalo Nicolás Peñaherrera Escobar <gonzalop67@gmail.com>
2020-07-02 00:18:58 -0500

    Modelo, Controlador Usuario

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UsuarioFormRequest;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        if ($request) {
+            $query = trim($request->get('searchText'));
+            $usuarios = DB::table('users')
+                ->where('name', 'LIKE', '%'.$query.'%')
+                ->orderBy('id', 'desc')
+                ->paginate(7);
+            return view('seguridad.usuario.index', ["usuarios" => $usuarios, "searchText" => $query]);
+        }
+    }
+
+    public function create()
+    {
+        return view("seguridad.usuario.create");
+    }
+
+    public function store(UsuarioFormRequest $request)
+    {
+        $usuario = new User();
+        $usuario->name = $request->get('name');
+        $usuario->email = $request->get('email');
+        $usuario->password = Hash::make($request->get('password'));
+        $usuario->save();
+
+        return redirect('seguridad/usuario');
+    }
+
+    public function edit($id)
+    {
+        return view("seguridad.usuario.edit", ["usuario" => User::findOrFail($id)]);
+    }
+
+    public function update(UsuarioFormRequest $request, $id)
+    {
+        $usuario = User::findOrFail($id);
+        $usuario->name = $request->get('name');
+        $usuario->email = $request->get('email');
+        $usuario->password = Hash::make($request->get('password'));
+        $usuario->update();
+
+        return redirect('seguridad/usuario');
+    }
+
+    public function destroy($id)
+    {
+        $usuario = DB::table('users')->where('id', '=', $id)->delete();
+
+        return redirect('seguridad/usuario');
+    }
+}
